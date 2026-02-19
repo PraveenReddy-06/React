c91313c5532b4a0faa008583b1ab6ec79bb0f457
PraveenReddy-06 <2400032662@kluniversity.in>
2026-02-19 22:29:24 +0530

    Import and export,{js}

---
+++ Import and export/app.js
@@ -0,0 +1,4 @@
+const a="hi im praveen";
+const arr=[1,2,3,4,5];
+export default arr; /* no matter how many var are there the thing we export as deafault gets exported*/
+export  {a}; /* we can export as many var as we want but only one default export is allowed */
---
+++ Import and export/script.js
@@ -0,0 +1,4 @@
+import arr from "./app.js"
+import {a} from "./app.js"/* script is must*/
+console.log(arr);
+console.log(a);
